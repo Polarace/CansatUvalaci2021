a81a4f2708bb66c3aa2a474fd1b0a8720ba9edac
Polarace <66486525+Polarace@users.noreply.github.com>
2021-06-28 22:04:01 +0200

    Motor driver library
    Added motor driver library support for Arduino IDE

---
+++ Software/rover/motor_driver_arduino_lib/motor_driver.h
@@ -0,0 +1,46 @@
+#include "stdint.h"
+#include "Arduino.h"
+
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+/* Important code */  
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+typedef struct 
+{
+    uint8_t             Direction;      /* Can be any of MOTOR_DIRECTION_X */
+
+    uint8_t             PinPlus;        /* Pin at which '+' is connected */ 
+
+    uint8_t             PinMinus;       /* Pin at which '-' is connected */ 
+
+} MotorDef;
+
+/* ************ Function prototypes ************ */
+
+/* Initialization function */
+void motor_init(MotorDef *motor);
+
+/* Forward move function */
+void motor_forward(MotorDef *motor);
+
+/* Backward move function */
+void motor_backward(MotorDef *motor);
+
+/* Stop motor function */
+void motor_stop(MotorDef *motor);
+
+/* Direction defines */
+#define MOTOR_DIRECTION_LEFT                0x00
+#define MOTOR_DIRECTION_RIGHT               0x01
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
